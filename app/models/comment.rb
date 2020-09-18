48c260af47fb2fdbb675191cc8f952dccd6aa037
Victor Motogna <vmotogna@gmail.com>
2020-09-18 13:48:40 +0300

    Revert "Implement add comments to posts"
    This reverts commit 1c4389304d22ec14b1d804065f728c14ca0b5108.

--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Comment < ApplicationRecord
-  belongs_to :post
-
-  validates :body, length: { minimum: 1, maximum: 1000 }
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  has_many :comments, dependent: :destroy
-
   validates :body, length: { minimum: 1, maximum: 1000 }
 end
