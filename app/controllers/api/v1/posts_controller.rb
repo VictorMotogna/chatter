b8a9d0fc22ae16e533c8b2e5bd74d9198a75eaac
Florin Ionce <ionce.florin@gmail.com>
2020-09-18 18:59:46 +0300

    Add websockets for the vue app

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::PostsController < ApplicationController
-  # include CableReady::Broadcaster
+  include CableReady::Broadcaster
 
   def index
     @posts = Post.order(created_at: :desc).limit(10)
@@ -11,14 +11,8 @@ class Api::V1::PostsController < ApplicationController
   def create
     post = Post.create(post_params)
 
-    # cable_ready['feed'].insert_adjacent_html(
-    #   selector: '#feed',
-    #   position: 'afterbegin',
-    #   html: render_to_string(partial: 'posts/post', locals: { post: post })
-    # )
-    # cable_ready.broadcast
+    ActionCable.server.broadcast('feed', post: post)
 
-    # redirect_to posts_path
     render json: post
   end
 
