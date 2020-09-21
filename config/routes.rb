a8eb3a6091289ec5fd488ca8ada5d13c3a669c4b
Florin Ionce <ionce.florin@gmail.com>
2020-09-21 14:32:06 +0300

    Add simple "rails" functionallity

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index create]
   root 'posts#index'
 
+  namespace :rails do
+    resources :posts, only: %i[index create] do
+      post :like, on: :member
+      post :repost, on: :member
+    end
+  end
+
   namespace :api do
     namespace :v1 do
       resources :posts, only: %i[index create] do
